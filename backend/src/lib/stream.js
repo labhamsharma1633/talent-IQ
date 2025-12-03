a8cce6ab36585414bf037a1798b509907d11c2d3
Labham Sharma <git config --global user.email>
2025-12-03 15:48:53 +0530

    auth & stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,34 @@
+import {StreamChat} from "stream-chat"
+import { ENV } from "./env.JS"
+const apiKey=ENV.STREAM_API_KEY
+const apiSecret=ENV.STREAM_API_SECRET
+if(!apiKey || !apiSecret){
+    console.error("Stream API KEY or SECRET is missing");
+
+}
+export const chatClient=StreamChat.getInstance(apiKey,apiSecret);
+
+
+export const upsertStreamUser=async(userData)=>{
+    try{
+        await chatClient.upsertUsers(userData);
+        console.log("Stream user upserted successfully: ",userData);
+        return userData
+    }
+    catch(error){
+        console.error("Error upSerting stream user: ",error)
+    }
+}
+
+export const deleteStreamUser=async(userId)=>{
+    try{
+        await chatClient.deleteUser(userId)
+        console.log("Stream user deleted Successfully",userId)
+        return userData
+    }
+    catch(error){
+        console.error("Error deleting stream user: ",error)
+    }
+}
+
+// todo: add another method to generateToken
